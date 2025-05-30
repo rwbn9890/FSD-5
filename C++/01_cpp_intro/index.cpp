e4217c63848029fc808f07af831f998b691b7825
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-05-30 12:37:19 +0530

    intro

---
+++ C++/01_cpp_intro/index.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+#define mynum = 400;
+
+int main(){
+   
+    int x;
+    int y;
+
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+
+    cout << "x + y = " << x+y << endl;
+    cout << "x - y = " << x-y << endl;
+    cout << "x * y = " << x*y << endl;
+    cout << "x / y = " << x/y << endl;
+    cout << "x % y = " << x%y << endl;
+
+}
+
