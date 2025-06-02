79dac124ed2063ddc3ed61a4a7e0d61714d8e219
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-06-02 11:07:53 +0530

    operatores

---
+++ C++/02_cpp_operators/index.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int a = 20;
+int b = 20;
+
+
+
+// cout << (a > b ) << endl;
+// cout << (a < b ) << endl;
+// cout << (a <= b ) << endl;
+// cout << (a >= b ) << endl;
+
+// cout << (a == b) << endl;
+
+
+
+// int x= 400;
+// int y = 200;
+
+// cout << (a == b && x==y) << endl; // 0
+// cout << (a == b || x==y) << endl; // 1
+
+// cout << !z << endl;
+
+int x = 20;
+int z = 10;
+
+
+
+// z += x; // z = z + x;
+
+// z -= x; // z = z-x;
+
+
+// int l = 199;
+// int w = 99;
+
+// l -= w; // l = l-w;
+
+// char  c = 'A';
+
+
+// cout << sizeof(l) << endl;
+// cout << sizeof(c) << endl;
+
+
+
+// x--; // x = x-1;
+// x--;
+// x--;
+
+// z++; // z = z+1;
+// z++;
+// z++;
+// z++;
+
+
+// cout << x << endl;
+// cout << z << endl;
+
+
+
+// cout << x-- << endl;
+
+// cout << --x << endl;
+
+
+
+
+// 10 + 2 * 4 - 2 / 2 - (10 + 10)
+// 10 + 8 - 2 / 2 
+// 10 + 8 - 1
+// 17;
+
+
+
+
+float n1 = 2.1;
+int n2 = 2.2;
+
+
+cout << n1 + (char) n2 << endl;
+
+
+
+    return 0;
+}
