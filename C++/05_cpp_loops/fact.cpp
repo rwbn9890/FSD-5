f63adbac40d8b775330682b062c16a5df52e6622
Alishan Shaikh <rwbn1.alishan.as@gmail.com>
2025-06-05 11:10:46 +0530

    flow chart andloops

---
+++ C++/05_cpp_loops/fact.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// int i = 1;  
+// int n = 5;
+// int fact = 1;
+
+// while(i<=n)
+// {
+//     fact = fact * i; // fact *= i
+//     i++;
+// }
+
+// cout << "factorial of " << n << ": " << fact << endl;
+
+
+
+
+int n = 5;
+int fact = 1;
+
+while (n>=1)
+{
+    fact *= n; // fact = fact * n;
+    n--;
+}
+
+cout << "factorial of n" <<": " << fact << endl;
+
+    return 0;
+}
